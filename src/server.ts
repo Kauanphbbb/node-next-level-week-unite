0b655907ac01290ba71aa02f24b3fb7d9572148a
Kauanphbbb <kauanphbbb@gmail.com>
2024-04-02 19:55:50 -0300

    first day

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import z from "zod";
 
 const app = fastify();
 
+const prisma = new PrismaClient();
+
 app.get("/", async () => {
-  return 'Hello, World!';
+  return "Hello, World!";
+});
+
+app.post("/events", async (request, reply) => {
+  try {
+    const createEventBody = z.object({
+      title: z.string(),
+      description: z.string().nullable(),
+      maximumAttendees: z.number().int().min(1),
+    });
+
+    const data = createEventBody.parse(request.body);
+
+    const event = await prisma.event.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        slug: data.title.toLowerCase().replaceAll(" ", "-"),
+        maximumAttendees: data.maximumAttendees,
+      },
+    });
+
+    return reply.status(201).send(event);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return reply.status(400).send(error.issues);
+    }
+  }
 });
 
 app.listen({ port: 3000 }, (err, address) => {
